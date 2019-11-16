3c43371dc85da07070168a9261e3f654b148db12
Taiga Nakano <gaitagaita69@gmail.com>
2019-11-16 11:00:59 +0900

    スライド準備中

--- script/script.js
+++ script/script.js
@@ -43,4 +43,15 @@ $(function(){
                 }, 500);
         }
     });
+
+     //トップ画像のスライド
+    // $('.bg-slider').bgSwitcher({
+    //     images: ['../img/top1.png','../img/top2.png','../img/top3.png'], // 切替背景画像を指定
+	//     interval: 5000, // 背景画像を切り替える間隔を指定 3000=3秒
+    //     loop: true, // 切り替えを繰り返すか指定 true=繰り返す　false=繰り返さない
+    //     shuffle: true, // 背景画像の順番をシャッフルするか指定 true=する　false=しない
+    //     effect: "blind", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
+    //     duration: 500, // エフェクトの時間を指定します。
+    //     easing: "swing" // エフェクトのイージングをlinear,swingから指定
+    // });
 });
