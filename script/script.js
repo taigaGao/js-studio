ba42440200e053c5983049e7c8eae8c2fc159eb7
Taiga Nakano <gaitagaita69@gmail.com>
2019-11-16 14:24:06 +0900

    提出前

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 $(function(){
-    //⑤コンテンツがスクロール時に浮き上がってくる仕様
+    //⑤コンテンツがスクロール時に浮き上がってくる仕様(ライブラリの使用)
     $(window).scroll(function (){
         $('.fadein').each(function(){
             var targetElement = $(this).offset().top;
@@ -44,7 +44,13 @@ $(function(){
         }
     });
 
-     //トップ画像のスライド
+    $('.logo').click(function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500);
+    });
+
+     //トップ画像のスライド(背景画像の切り替え)
     // $('.bg-slider').bgSwitcher({
     //     images: ['../img/top1.png','../img/top2.png','../img/top3.png'], // 切替背景画像を指定
 	//     interval: 5000, // 背景画像を切り替える間隔を指定 3000=3秒
@@ -54,4 +60,55 @@ $(function(){
     //     duration: 500, // エフェクトの時間を指定します。
     //     easing: "swing" // エフェクトのイージングをlinear,swingから指定
     // });
+
+    //トップ画像のスライド(背景画像の切り替え)
+    // $('#slide').slide({
+    //     interval_normal : 5000,
+    // });
+
+    //トップ画像のスライド(時間切り替え)
+    setInterval(function(){
+        if($('.slide--one').hasClass('active')){
+            $('.slide--one').removeClass('active');
+            $('.slide--two').addClass('active');
+        } else if ($('.slide--two').hasClass('active')){
+            $('.slide--two').removeClass('active');
+            $('.slide--three').addClass('active');
+        } else {
+            $('.slide--three').removeClass('active');
+            $('.slide--one').addClass('active');
+        }
+    },5000);
+    //トップ画像のスライド(ボタン切り替え)
+    $('.index__btn--one').click(function() {
+        $(this).parent().parent().removeClass('active');
+        $('.slide--one').addClass('active');
+    });
+    $('.index__btn--two').click(function() {
+        $(this).parent().parent().removeClass('active');
+        $('.slide--two').addClass('active');
+    });
+    $('.index__btn--three').click(function() {
+        $(this).parent().parent().removeClass('active');
+        $('.slide--three').addClass('active');
+    });
+
+
+    $('.angle-icon').click(function() {
+        var $answer = $(this).parent().parent().find('.faq__item--ans');
+        if($answer.hasClass('open')) {
+            $answer.removeClass('open');
+            $(this).parent().find('.fa-angle-up').hide();
+            $(this).parent().find('.fa-angle-down').show();
+            $answer.slideUp();
+        } else {
+            $answer.addClass('open'); 
+            $(this).parent().find('.fa-angle-down').hide();
+            $(this).parent().find('.fa-angle-up').show();
+            $answer.slideDown();
+        }
+    });
+
+
+
 });
